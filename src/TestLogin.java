93eaafff574fdb2cddfa73518656b6265ae5b3ef
Fawk <ok222ax@student.lnu.se>
2015-11-26 10:46:24 +0100

    RED

---
+++ src/TestLogin.java
@@ -0,0 +1,25 @@
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.mockito.Mockito.*;
+import org.junit.Assert.*;
+
+public class TestLogin {
+	
+	private String username = "admin";
+	private String password = "root";
+	
+	Login mockedLogin = null;
+
+	@Before
+	public void setUp() throws Exception {
+		mockedLogin = mock(Login.class);
+	}
+
+	@Test
+	public void TestLoginCredentials() {
+		assertTrue(mockedLogin.Login(username, password));
+	}
+
+}
