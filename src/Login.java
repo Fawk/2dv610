9dc51b63262e88695768c8ec1b97d6d76118b698
Fawk <ok222ax@student.lnu.se>
2015-11-26 12:20:35 +0100

    RED/REFACTOR

--- src/Login.java
+++ src/Login.java
@@ -1,6 +1,54 @@
+import java.util.HashMap;
+
 
 public class Login {
+	
+	private boolean hasError = false;
+	private String loginError = "You need to type in a username and password combination!";
+	private String faultyLogin = "Wrong username or password combination!";
+	private String currentError = null;
+	
+	private HashMap<String, String> users;
+	
+	public Login() {
+		 this.users = new HashMap<String, String>();
+		 this.users.put("admin", "root");
+	}
+	
 	public boolean Login(String user, String pass) {
+		if(user == "" || pass == "") {
+			this.hasError = true;
+			this.currentError = loginError;
+			return false;
+		}
+		return Validate(user, pass);
+	}
+	
+	private boolean Validate(String user, String pass) {
+		if(this.users.containsKey(user) && this.users.get(user) == pass) {
+			this.hasError = false;
+			this.currentError = null;
+			return true;
+		}
+		
+		this.hasError = true;
+		this.currentError = faultyLogin;
 		return false;
+	}
+	
+	public boolean HasError() {
+		return this.hasError && this.currentError != null;
+	}
+	
+	public String GetError() {
+		return this.currentError;
+	}
+	
+	public String GetLoginError() {
+		return this.loginError;
+	}
+	
+	public String GetFaultyLogin() {
+		return this.faultyLogin;
 	}
 }
--- src/TestLogin.java
+++ src/TestLogin.java
@@ -7,39 +7,49 @@ import org.junit.Assert.*;
 
 public class TestLogin {
 	
-	private String username = "admin";
-	private String password = "root";
-	
-	Login mockedLogin = null;
+	User mockedUser = null;
+	Login login = null;
 
 	@Before
 	public void setUp() throws Exception {
-		mockedLogin = mock(Login.class);
-		when(mockedLogin.Login(username, password)).thenReturn(true);
+		login = new Login();
+		mockedUser = mock(User.class);
+		when(login.Login(mockedUser)).thenReturn(true);
 	}
 
 	@Test
-	public void TestLoginCredentials() {
-		assertTrue(mockedLogin.Login(username, password));
-		verify(mockedLogin).Login(username, password);
+	public void TestMockedLogin() {
+		assertTrue(login.Login(mockedUser));
+		verify(login).Login(mockedUser);
 	}
 	
 	@Test
 	public void TestLoginError() {
-		assertFalse(mockedLogin.Login("", ""));
-		verify(mockedLogin).Login("", "");
-		assertTrue(mockedLogin.hasError);
-		assertEquals(mockedLogin.GetError(), mockedLogin.loginError);
-		verify(mockedLogin).GetError();
+		
+		User faultyUser = new User("test", "test");
+		
+		assertFalse(login.Login(faultyUser));
+		verify(login).Login(faultyUser);
+		assertTrue(login.HasError());
+		verify(login).HasError();
+		assertEquals(login.GetError(), login.GetLoginError());
+		verify(login).GetError();
+		verify(login).GetLoginError();
 	}
 	
 	@Test
 	public void TestFaultyLogin() {
-		assertFalse(mockedLogin.Login("test", "test"));
-		verify(mockedLogin).Login("test", "test");
-		assertTrue(mockedLogin.hasError);
-		assertEquals(mockedLogin.GetError(), mockedLogin.faultyLogin);
-		verify(mockedLogin).GetError();
+		
+		User faultyUser = new User("", "");
+		
+		assertFalse(login.Login(faultyUser));
+		verify(login).Login(faultyUser);
+		assertTrue(login.HasError());
+		verify(login).HasError();
+		assertEquals(login.GetError(), login.GetFaultyLogin());
+		verify(login).GetError();
+		verify(login).GetFaultyLogin();
 	}
-
+	
+	
 }
